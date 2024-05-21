1c9e2124a7d678a7348b8da3fe08de00719f3d17
kiendt <kiendt@omegatheme.com>
2024-05-21 14:05:40 +0700

    .

---
+++ assets/discount-tab.js
@@ -0,0 +1,23 @@
+class DiscountTab extends HTMLElement {
+  constructor() {
+    super();
+    // this.cachedResults = {};
+    // this.predictiveSearchResults = this.querySelector('[data-predictive-search]');
+    // this.allPredictiveSearchInstances = document.querySelectorAll('predictive-search');
+    // this.isOpen = false;
+    // this.abortController = new AbortController();
+    // this.searchTerm = '';
+
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    // this.input.form.addEventListener('submit', this.onFormSubmit.bind(this));
+    // this.input.addEventListener('focus', this.onFocus.bind(this));
+    // this.addEventListener('focusout', this.onFocusOut.bind(this));
+    // this.addEventListener('keyup', this.onKeyup.bind(this));
+    // this.addEventListener('keydown', this.onKeydown.bind(this));
+  }
+}
+
+customElements.define('discount-tab', DiscountTab);
